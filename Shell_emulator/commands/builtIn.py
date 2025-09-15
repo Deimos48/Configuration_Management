a13890f65b367f1b818ec32aec3260090b8df44a
MAXWORK\Deimos <maxsedas@gmail.com>
2025-09-15 09:59:01 +0300

    Этап 1. Полная реализация заглушек

--- Shell_emulator/commands/builtIn.py
+++ Shell_emulator/commands/builtIn.py
@@ -6,10 +6,20 @@ def cmd_exit(args: list[str]) -> bool:
 
 def cmd_help(args: list[str]) -> bool:
     # Печать справки
-    print("Доступные команды: help, exit")
+    print("Доступные команды: help, exit, echo")
     return True
 
 def cmd_echo(args: list[str]) -> bool:
     # Вывод текста
     print(" ".join(args))
+    return True
+
+def cmd_ls(args: list[str]) -> bool:
+    # Вывод окружающих файлов (Заглушка)
+    print("ls " + " ".join(args))
+    return True
+
+def cmd_cd(args: list[str]) -> bool:
+    # Переход по файлам (Заглушка)
+    print("cd " + " ".join(args))
     return True
--- Shell_emulator/repl.py
+++ Shell_emulator/repl.py
@@ -1,31 +1,44 @@
+import getpass
+import shlex
+import socket
+
 from commands import builtIn
 
 COMMANDS = {
     "exit": builtIn.cmd_exit,
     "help": builtIn.cmd_help,
     "echo": builtIn.cmd_echo,
+    "ls": builtIn.cmd_ls,
+    "cd":builtIn.cmd_cd
 
 }
 
-def handle_command(cmd: str) -> bool:
-    parts = cmd.split()
-    name, args = parts[0], parts[1:]
-
-    if (name in COMMANDS):
-        return COMMANDS[name](args)
-    else:
-        print(f"[DEBUG] Команда {name} не поддерживается")
+def handle_command(cmd_line: str) -> bool:
+    try:
+        parts = shlex.split(cmd_line)
+        if not parts:
+            return True  # пустой ввод
+        name, args = parts[0], parts[1:]
+        if name in COMMANDS:
+            return COMMANDS[name](args)
+        else:
+            print(f"Ошибка: неизвестная команда '{name}'")
+            return True
+    except ValueError as e:
+        print(f"Ошибка парсинга команды: {e}")
         return True
 
 
 def repl():
-    print("Мини-оболочка.\nВведите 'help' для справки, 'exit' для выхода")
+    print("Мини-оболочка. Введите 'help' для справки, 'exit' для выхода")
+    username = getpass.getuser()
+    hostname = socket.gethostname()
+
     while True:
         try:
-            cmd = input(">>> ").strip()
-            if not cmd:
-                continue
-            if not handle_command(cmd):
+            prompt = f"{username}@{hostname}:~$ "
+            cmd_line = input(prompt)
+            if not handle_command(cmd_line):
                 break
         except (KeyboardInterrupt, EOFError):
             print("\nВыход из эмулятора")
